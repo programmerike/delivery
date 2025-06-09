f8f00c3e7afddc9234707c79708ebd0f98651994
Isaac <isaac1700mensah@gmail.com>
2025-06-09 06:51:17 -0500

    Initial Commit

--- src/components/DeliveryForm.jsx
+++ src/components/DeliveryForm.jsx
@@ -25,7 +25,7 @@ export default function DeliveryForm() {
     }
     return new Promise((resolve, reject) => {
       const script = document.createElement("script");
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&libraries=places&v=weekly`;
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.GOOGLE_MAPS_API_KEY}&libraries=places&v=weekly`;
       script.async = true;
       script.defer = true;
       script.onload = () => resolve();
